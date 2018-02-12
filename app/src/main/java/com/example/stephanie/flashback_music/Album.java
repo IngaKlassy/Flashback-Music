d7388c9d03d265b32eb34dbb0fbe1928a809f608
Amanda Moffitt <amoffitt@ucsd.edu>
2018-02-12 12:05:31 -0800

    Made a few changes in album

--- app/src/main/java/com/example/stephanie/flashback_music/Album.java
+++ app/src/main/java/com/example/stephanie/flashback_music/Album.java
@@ -1,8 +1,11 @@
+/**
+ * Created by Stephanie on 2/6/2018.
+ */
+
 package com.example.stephanie.flashback_music;
 
 import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
-
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -10,16 +13,13 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-/**
- * Created by Stephanie on 2/6/2018.
- */
 
 public class Album {
     private String albumTitle;
     private String albumArtist;
 
-    ArrayList<Song> songObList;
-    List<String> songTitleList;
+    private ArrayList<Song> songObList;
+    private List<String> songTitleList;
 
     public Album (String title, String artist) {
         albumTitle = title;
@@ -56,7 +56,6 @@ public class Album {
     {
         return songTitleList;
     }
-
 
     String getAlbumTitle() {
         return albumTitle;
