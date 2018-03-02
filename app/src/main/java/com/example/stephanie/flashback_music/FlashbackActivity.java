a37f9f002eb4f89a9bbee79434c12713b6baab09
Muyao Wu <muw006@ucsd.edu>
2018-03-02 15:47:16 -0800

    MS2 US1 Task1 #31 Added login feature

--- app/src/main/java/com/example/stephanie/flashback_music/FlashbackActivity.java
+++ app/src/main/java/com/example/stephanie/flashback_music/FlashbackActivity.java
@@ -1,11 +1,22 @@
 package com.example.stephanie.flashback_music;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 
@@ -13,6 +24,7 @@ import static com.example.stephanie.flashback_music.MainActivity.mainActivityPla
 
 public class FlashbackActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 1;
     CompoundButton flashbackSwitch;
 
     ArrayList<TextView> textviews;
@@ -22,11 +34,21 @@ public class FlashbackActivity extends AppCompatActivity {
     TextView songArtist;
     TextView lastPlayed;
 
+    GoogleSignInClient mGoogleSignInClient;
+    GoogleSignInAccount myAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashback);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
 
         songName = (TextView) findViewById(R.id.song_title);
         songAlbum = (TextView) findViewById(R.id.song_album);
@@ -76,6 +98,22 @@ public class FlashbackActivity extends AppCompatActivity {
             });
         }
 
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+                    // ...
+                }
+            }
+        });
+
+
 
         flashbackSwitch = (CompoundButton) findViewById(R.id.flashback_switch);
 
@@ -89,8 +127,57 @@ public class FlashbackActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+    }
+    private void signIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            Toast.makeText(getApplicationContext(), account.toString(), Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Not logged in", Toast.LENGTH_LONG).show();
+        }
+        //TODO: UPDATEUI
+
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            myAccount = account;
+
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            //Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            //updateUI(null);
+        }
+    }
     public void displayNoSongsToPlay(ArrayList<TextView> textviews) {
         textviews.get(0).setText("No songs played");
 
